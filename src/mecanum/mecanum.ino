a5bf76f2d3a9b0d61f3e31110772506719eb0670
TaiyouKomazawa <e1611216@toyama.kosen-ac.jp>
2020-11-18 15:24:50 +0900

    制御周期の調整

--- src/mecanum/mecanum.ino
+++ src/mecanum/mecanum.ino
@@ -72,10 +72,10 @@ void init_qei()
 void init_pid()
 {
                         /*kp    ki    kd   ctrl_freq*/
-  pid[0] = new FastPID  (1.1, 0.5, 0.0001, 200 /*Hz*/, true);
-  pid[1] = new FastPID  (1.1, 0.5, 0.0001, 200 /*Hz*/, true);
-  pid[2] = new FastPID  (1.1, 0.5, 0.0001, 200 /*Hz*/, true);
-  pid[3] = new FastPID  (1.1, 0.5, 0.0001, 200 /*Hz*/, true);
+  pid[0] = new FastPID  (1.1, 0.5, 0.0001, 50 /*Hz*/, true);
+  pid[1] = new FastPID  (1.1, 0.5, 0.0001, 50 /*Hz*/, true);
+  pid[2] = new FastPID  (1.1, 0.5, 0.0001, 50 /*Hz*/, true);
+  pid[3] = new FastPID  (1.1, 0.5, 0.0001, 50 /*Hz*/, true);
 }
 
 void init_wheels()
